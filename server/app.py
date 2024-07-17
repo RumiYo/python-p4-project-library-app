9978e143085c9a42592d09a8a53a0057a13c5c6c
RumiYo <yokoyamarumi@gmail.com>
2024-07-17 12:14:32 -0400

    added models

--- server/app.py
+++ server/app.py
@@ -3,13 +3,13 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
+from flask import make_response, request
 from flask_restful import Resource
 
 # Local imports
 from config import app, db, api
 # Add your model imports
-
+from models import Book, Loan, Member
 
 # Views go here!
 
--- server/models.py
+++ server/models.py
@@ -4,3 +4,50 @@ from sqlalchemy.ext.associationproxy import association_proxy
 from config import db
 
 # Models go here!
+class Book(db.Model, SerializerMixin):
+    __tablename__ = 'books'
+    serialize_rules = ('-loans.book', '-members.books','-members.loans')
+
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String, nullable=False)
+    author = db.Column(db.String, nullable=False)
+    publish_year = db.Column(db.Integer)
+    page = db.Column(db.Integer)
+    image_url = db.Column(db.String)
+    summary = db.Column(db.String)
+
+    # Add relationships
+    loans = db.relationship('Loan', back_populates='book')
+    members = association_proxy('loans', 'member',
+                    creator=lambda project_obj: Loan(project=project_obj))
+
+
+class Loan(db.Model, SerializerMixin):
+    __tablename__ = 'loans'
+    serialize_rules = ('-book.loans', '-member.books','member.loans')
+
+    id = db.Column(db.Integer, primary_key=True)
+    loan_date = db.Column(db.DateTime, server_default=db.func.now())
+    returned_date = db.Column(db.DateTime)
+    book_id = db.Column(db.Integer, db.ForeignKey('books.id'))
+    member_id = db.Column(db.Integer, db.ForeignKey('members.id'))
+    # Add relationships
+    book = db.relationship('Book', back_populates='loans')
+    member = db.relationship('Member', back_populates='loans')
+
+
+class Member(db.Model, SerializerMixin):
+    __tablename__='member'
+    serialize_rules = ('-loans.member','-books.members', '-books.loans')
+
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String, nullable=False)
+    last_name = db.Column(db.String, nullable=False)
+    user_id = db.Column(db.String, nullable=False, unique=True)
+    # password = 
+    email = db.Column(db.String, nullable=False)
+
+    # Add relationships
+    loans = db.relationship('Loan', back_populates='member')
+    books = association_proxy('loans', 'book',
+                creator=lambda project_obj: Loan(project=project_obj))
