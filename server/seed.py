afc0e468649f1ee8b601a7c9cfb666937564f6c7
RumiYo <yokoyamarumi@gmail.com>
2024-07-17 13:02:34 -0400

    add create_members on seed.py

--- server/seed.py
+++ server/seed.py
@@ -5,13 +5,38 @@ from random import randint, choice as rc
 
 # Remote library imports
 from faker import Faker
+import random
 
 # Local imports
 from app import app
-from models import db
+from models import db, Book, Member, Loan
+
+fake = Faker()
+
+def create_members():
+    members = []
+    for _ in range(10):
+        m = Member(
+            first_name = fake.first_name(),
+            last_name = fake.last_name(),
+            email = fake.email()         
+        )
+        m.user_id = f"{m.first_name}{random.randint(10,100)}"
+        members.append(m)
+    return members
 
 if __name__ == '__main__':
-    fake = Faker()
+    
     with app.app_context():
+        print('Cleaning db...')
+        Member.query.delete()
+
         print("Starting seed...")
         # Seed code goes here!
+
+        print('Seeding members...')
+        members = create_members()
+        db.session.add_all(members)
+        db.session.commit()
+
+        print("Done seeding!")
