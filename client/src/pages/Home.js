03bd88d7a1acdbdc15cfeb33bfec6c597f04d436
RumiYo <yokoyamarumi@gmail.com>
2024-07-21 14:51:13 -0400

    add details on Home

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,6 +3,8 @@ function Home(){
     return(
         <body>
          <h1>Welcome back to Rumi Online Library</h1>
+         <img src="https://png.pngtree.com/thumb_back/fh260/background/20230705/pngtree-realistic-3d-rendering-of-laptop-accessing-online-library-image_3797278.jpg" alt="library" id="topImage"/>
+         <p>Welcome to Rumi Online Library! <br/><br/>Dive into a world of boundless knowledge and endless stories with our extensive collection of digital books. Whether you’re a student, a casual reader, or a literary enthusiast, Rumi Online Library is your gateway to an enriching reading experience. Explore thousands of titles across various genres and subjects, all at your fingertips.<br/><br/> Join our community of book lovers and embark on your next literary adventure today!</p>
         </body>
     )
 
